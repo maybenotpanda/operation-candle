a1f98ce3ae485b7429d452848e9c104fc4324492
maybenotpanda <maybenotpanda@gmail.com>
2025-09-30 21:04:48 +0700

    feat-alert: create new layout elements

--- src/components/elements/alert/index.jsx
+++ src/components/elements/alert/index.jsx
@@ -1,10 +1,16 @@
 // ** React Imports
 import React from 'react'
 
-const Alert = () => {
+const Alert = ({ button, children }) => {
   return (
-    <div>Alert</div>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96 text-center">
+        <h2 className="font-pixel mb-4">Game Over</h2>
+        {button ? children : null}
+      </div>
+    </div>
   )
 }
 
 export default Alert
+// <p className="mb-6">You answered incorrectly or ran out of time!</p>
